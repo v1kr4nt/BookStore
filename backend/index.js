d65177f7b525c872e43154a31c9bdc7b3cb70e7f
vikrant <notvikrant80@gmail.com>
2023-09-01 14:14:37 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoURI } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
-});
+app.get("/");
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("App is connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
